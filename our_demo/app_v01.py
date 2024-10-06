ded6d9d7a72db8caaa60c9b87ae7f2570d903dfb
ko0ong <2023020917@korea.ac.kr>
2024-10-06 19:53:16 +0900

    Update app_v01.py

--- our_demo/app_v01.py
+++ our_demo/app_v01.py
@@ -78,7 +78,7 @@ ADJECTIVES = read_word_list(ADJECTIVE_FILE)
 
 def calculate_korean_gpt_probability(text):
     score = 0
-    total_checks = 4  # 체크 항목 수
+    total_checks = 100  # 체크 항목 수
     text_length = len(text)
 
     # 텍스트 길이에 따른 기준 설정
@@ -90,24 +90,40 @@ def calculate_korean_gpt_probability(text):
     sentences = [s.strip() for s in sentences if s.strip()]  # 빈 문장 제거
     sentences_with_many_commas = [s for s in sentences if s.count(',') >= 2]
     if len(sentences_with_many_commas) / len(sentences) > 0.1:  # 10% 이상의 문장이 쉼표 2개 이상이면
-        score += 1
+        score += 20
 
     # 자주 등장하는 부분 체크
     korean_connectives = ['시절,', '맡았으며,', '특히', '우선,', '입사 후,', '에서,', '이에 따라', '바탕으로', '저는', '고,', '이는', '통해']
     connective_count = sum(text.count(word) for word in korean_connectives)
     if connective_count > 2 * length_factor:  # 텍스트 길이에 비례하여 조정
-        score += 3
+        score += 35
 
     # 긴 문장 체크
     if sentences:
         long_sentences = [s for s in sentences if len(s.split()) > 20]
         if len(long_sentences) > len(sentences) * length_factor / 2:  # 텍스트 길이에 비례하여 조정
-            score += 2
+            score += 15
 
     # 부사 사용 체크
     adverb_count = sum(text.count(adverb) for adverb in ADVERBS)
     if adverb_count >= 2 * length_factor:  # 텍스트 길이에 비례하여 조정
-        score += 2
+        score += 15
+
+    # 명사 사용 체크
+    noun_count = sum(text.count(noun) for noun in NOUNS)
+    if noun_count >= 2 * length_factor:  # 텍스트 길이에 비례하여 조정
+        score += 5
+
+    # 동사 사용 체크
+    verb_count = sum(text.count(verb) for verb in VERBS)
+    if verb_count >= 2 * length_factor:  # 텍스트 길이에 비례하여 조정
+        score += 5
+
+    # 형용사 사용 체크
+    adj_count = sum(text.count(adj) for adj in ADJECTIVES)
+    if adj_count >= 2 * length_factor:  # 텍스트 길이에 비례하여 조정
+        score += 5
+    
 
     # 텍스트 길이에 따른 점수 조정
     normalized_score = score * (1 - 0.2 * length_factor)  # 텍스트가 길수록 점수를 약간 낮춤
